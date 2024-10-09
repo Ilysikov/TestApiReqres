313152827827d73a4d3b237fdbb2a728878a82cb
Иван Лысиков <ivan.lysikov@fojin.tech>
2024-10-09 12:48:44 +0300

    commit_jenkins

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 
 if __name__ == "__main__":
     # os.system("python3 -m pytest")
-    os.system("poetry run pytest test")
-    # --alluredir allure - results
-    # os.system("allure serve allure-results")
+    os.system("poetry run pytest test --alluredir allure - results")
+    #
+    os.system("allure serve allure-results")
